22d04a4e57f23054b8b9a664ef37462b0bab4765
Matthew King <mattking73@gmail.com>
2018-03-03 21:50:22 -0800

    Add basic layout for blog

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,14 @@
 module Main exposing (main)
 
-import Html exposing (..)
+import Html exposing (Html, body, section, div, text, h1, p, img, label, a)
+import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
+import List exposing (map)
 
 
 main =
     Html.beginnerProgram
-        { model = home
+        { model = model
         , update = update
         , view = view
         }
@@ -18,28 +20,26 @@ main =
 
 type alias Model =
     { title : String
-    , content : String
+    , picture : String
+    , posts : List BlogPost
+    }
+
+
+type alias BlogPost =
+    { title : String
+    , description : String
     }
 
 
 model =
-    Nothing
+    { title = "making3's Software Blog"
+    , picture = "https://secure.gravatar.com/avatar/1e84d7b396211e9c7bbd888dc51249a4?s=188"
+    , posts = []
+    }
 
 
 
 -- UPDATE
-
-
-home =
-    { title = "Matt King's blog of software shenanigans"
-    , content = "What I learned in software school is..."
-    }
-
-
-whatIMadeWithElmPost =
-    { title = "What I made with Elm"
-    , content = "Here's the blog I made in Elm:..."
-    }
 
 
 type Msg
@@ -47,25 +47,83 @@ type Msg
     | ShowWhatIMadeWithElmPost
 
 
+update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        ShowHomePage ->
-            home
-
-        ShowWhatIMadeWithElmPost ->
-            whatIMadeWithElmPost
+    model
 
 
 
+-- case msg of
+--     ShowHomePage ->
+--         home
+--
+--     ShowWhatIMadeWithElmPost ->
+--         whatIMadeWithElmPost
 -- VIEW
+-- body []
+--     [ h1 [] [ text model.title ]
+--     , text model.description
+--     , div []
+--         [ button [ onClick ShowHomePage ] [ text "Home" ]
+--         , button [ onClick ShowWhatIMadeWithElmPost ] [ text "What I made with Elm" ]
+--         ]
+--     ]
 
 
+view : Model -> Html Msg
 view model =
     body []
-        [ h1 [] [ text model.title ]
-        , text model.content
-        , div []
-            [ button [ onClick ShowHomePage ] [ text "Home" ]
-            , button [ onClick ShowWhatIMadeWithElmPost ] [ text "What I made with Elm" ]
+        [ div [ class "section" ]
+            [ div [ class "container" ]
+                [ h1 [ class "title" ]
+                    [ text model.title ]
+                , div [ class "columns" ]
+                    [ div
+                        [ class "column is-one-quarter" ]
+                        [ div [ class "level" ]
+                            [ img [ class "profile-me", src model.picture ] [] ]
+                        , div [ class "level" ]
+                            [ text "Matthew King" ]
+                        ]
+                    , div [ class "column is-three-quarters" ]
+                        [ viewHome model ]
+                    ]
+                ]
             ]
         ]
+
+
+viewHome : Model -> Html Msg
+viewHome model =
+    div [] (listBlogPosts model.posts)
+
+
+listBlogPosts : List BlogPost -> List (Html Msg)
+listBlogPosts blogPosts =
+    map
+        (\l ->
+            a [ class "blog-post-item" ]
+                [ p [ class "title is-6" ] [ text l.title ]
+                , p [ class "subtitle is-6" ] [ text l.description ]
+                ]
+        )
+        getBlogPosts
+
+
+getBlogPosts : List BlogPost
+getBlogPosts =
+    [ softwareSchool, whatIMadeWithElmPost ]
+
+
+softwareSchool : BlogPost
+softwareSchool =
+    { title = "Matt King's Software School"
+    , description = "What I learned in software school is..."
+    }
+
+
+whatIMadeWithElmPost : BlogPost
+whatIMadeWithElmPost =
+    { title = "What I made with Elm"
+    , description = "Here's the blog I made in Elm:..."
+    }
