c8a07979ae6831cd3dc50e8ca5ec0e03c5ba8968
Matthew King <mattking73@gmail.com>
2018-10-20 21:55:54 -0700

    Revert anchor link to markdown link (no longer working)

--- src/About.elm
+++ src/About.elm
@@ -12,7 +12,6 @@ view =
         [ Markdown.toHtml [ class "content" ] getAbout
         ]
 
-
 getAbout : String
 getAbout =
     """
@@ -20,14 +19,14 @@ getAbout =
 ## Who am I?
 
 
-Hi! I'm Matthew King. I'm a software developer currently working for <a href="https://www.iwthealth.com/" target="_blank">iWT Health</a> for a variety of backend services.
+Hi! I'm Matthew King. I'm a software developer currently working for [iWT Health](https://www.iwthealth.com/) for a variety of backend services.
   My _cool_ Github name came from my university, which I ended up creating my Stack Overflow and Github profiles from (for no real reason),
   which I stuck with somewhat. Since then, I've been _nicknamed_ matth due to a past colleague shortening my name as Matth instead of Matt (from Matthew), so I've embraced it the "joke" of it.
 
 I started my journey as a co-op student at iWT Health back in 2009 and have been fascinated by working
   on the layer of software that runs behind the front of the business. I started writing C# services and backends (with tiny front-ends),
   which eventually lead to Node.js as a integration-services layer. Although I still work with Node.js, I've been exploring functional programming,
-  inspired by a fellow co-worker, <a href="https://absynce.github.io/" target="_blank">absynce</a>, which brought Erlang, Elixir, and Elm to my attention.
+  inspired by a fellow co-worker, [absynce](https://absynce.github.io/), which brought Erlang, Elixir, and Elm to my attention.
 
 As for a personal note, I don't have the best range of hobbies, but mostly includes gaming / walking / running / binging on Netflix. Otherwise, my Wife and I recently moved from the
   midwest to the bay area to pursue her career, and we've been exploring all sorts of places in California!
@@ -35,15 +34,15 @@ As for a personal note, I don't have the best range of hobbies, but mostly inclu
 ## Contact / Social Stuff
 
 
-- <a href="https://twitter.com/making_3" target="_blank">Twitter</a>
-- <a href="https://github.com/making3" target="_blank">GitHub</a>
+- [Twitter](https://twitter.com/making_3)
+- [GitHub](https://github.com/making3)
 - <mailto:making3dev@gmail.com>
-- <a href="https://stackoverflow.com/users/724591/matth?tab=profile" target="_blank">Stack Overflow</a>
+- [Stack Overflow](https://stackoverflow.com/users/724591/matth?tab=profile)
 
 ## Acknowledgements
 
-- <a href="https://absynce.github.io/" target="_blank">Jared Smith (absynce)</a>
-- <a href="http://www.aowens.me/" target="_blank">Alex Owens</a>
+- [Jared Smith (absynce)](https://absynce.github.io/)
+- [Alex Owens](http://www.aowens.me/)
 - Josh Miller (first mentor) - For guiding me with programming early as a co-op student.
 
 """
